f3be92685085e2abdf87fe8ddad2a2682142a8d6
Jhon Doe <swustinowegor@gmail.com>
2026-05-05 18:43:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4661,4 +4661,4 @@ def find_restaurant_by_any_means(place_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=False)
